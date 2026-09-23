41c7c3c6499be7952394e0f992010d82b612f0df
Sergio Fernando Calderon <calderonsergio645@gmail.com>
2026-09-23 11:26:01 -0500

    Crear componentes home, barra de navegacion, footer, carpetas servicios y entidades

---
+++ src/app/navegacion/navegacion.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-navegacion',
+  styleUrl: './navegacion.css',
+  templateUrl: './navegacion.html',
+})
+export class Navegacion {}
