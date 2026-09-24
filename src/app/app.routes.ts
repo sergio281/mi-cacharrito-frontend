96e28e8cd51260db8c5472bd031c10abf260f1ff
Sergio Fernando Calderon <calderonsergio645@gmail.com>
2026-09-24 15:55:10 -0500

    Login de usuario, página de usuario y mejoras del inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Registro } from './registro/registro';
+import { Login } from './login/login';
+import { UsuarioComponent } from './usuario/usuario';
 
 export const routes: Routes = [
     { path: '', component: Home },
-    { path: 'registro', component: Registro }
+    { path: 'registro', component: Registro },
+    { path: 'login', component: Login },
+    { path: 'usuario', component: UsuarioComponent }
 ];
---
+++ src/app/usuario/usuario.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario',
+  standalone: true,
+  templateUrl: './usuario.html',
+  styleUrls: ['./usuario.css']
+})
+export class UsuarioComponent { }
